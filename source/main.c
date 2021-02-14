4a3f399349b0a16d71d5160b5ff92a338403fea8
LadyNick <nnikki1013@gmail.com>
2021-02-14 11:44:42 -0800

    changes almost done ex 1

--- source/main.c
+++ source/main.c
@@ -15,7 +15,7 @@
 void set_PWM(double frequency){
 	static double current_frequency;
 
-	if(frequency != current_frequence){
+	if(frequency != current_frequency){
 		if(!frequency) {TCCR3B &= 0X08;}
 		else { TCCR3B |= 0x03; }
 
@@ -23,7 +23,7 @@ void set_PWM(double frequency){
 
 		else if(frequency > 31250) { OCR3A = 0X0000; }
 
-		else { OCR3A = (short) (8000000 / (128*frequency)) = 1; }
+		else { OCR3A = (short) (8000000 / (128*frequency)) - 1; }
 
 		TCNT3 = 0;
 		current_frequency = frequency;
@@ -44,110 +44,91 @@ void PWM_off(){
 	TCCR3B = 0X00;
 }
 
-enum Notes_state(button1, button2, button3, silent, init)Note_state;
+enum Notes_states{button1, button2, button3, silent, init}Note_state;
 
 unsigned char A0;
 unsigned char A1;
 unsigned char A2;
 
-Tick_Note(){
+void Tick_Note(){
 	switch(Note_state){
 		case init:
 			Note_state = silent;
 			break;
 		case silent:
-			PWM_off();
+			set_PWM(0);
 			if(!A0 && !A1 && !A2){
 				Note_state = silent;
 			}
 			else if( A0 && !A1 && !A2){
 				Note_state = button1;
-				PWM_on();
 			}
 			else if( !A0 && A1 && !A2){
 				Note_state= button2;
-				PWM_on();
 			}
 			else if( !A0 && !A1 && A2 ){
 				Note_state= button3;
-				PWM_on();
 			}
 			else if( (A0 + A1 + A2) > 1 ){
-				PWM_off();
 			        Note_state= silent;
 			}	       
 			break;
 		case button1:
 			set_PWM(261.63);
 			if(!A0 && !A1 && !A2){
-				PWM_off();
 				Note_state= silent;
 			}
 			else if( A0 && !A1 && !A2){
 				Note_state= button1;
-				PWM_on();
 			}
 			else if( !A0 && A1 && !A2){
 				Note_state= button2;
-				PWM_on();
 			}
 			else if( !A0 && !A1 && A2 ){
 				Note_state= button3;
-				PWM_on();
 			}
 			else if( (A0 + A1 + A2) > 1 ){
-				PWM_off();
 			        Note_state= silent;
 			}
 			break;
 		case button2:
 			set_PWM(293.66);
 			if(!A0 && !A1 && !A2){
-				PWM_off();
 				Note_state= silent;
 			}
 			else if( A0 && !A1 && !A2){
 				Note_state= button1;
-				PWM_on();
 			}
 			else if( !A0 && A1 && !A2){
 				Note_state= button2;
-				PWM_on();
 			}
 			else if( !A0 && !A1 && A2 ){
 				Note_state= button3;
-				PWM_on();
 			}
 			else if( (A0 + A1 + A2) > 1 ){
-				PWM_off();
 			        Note_state= silent;
 			}
 			break;
 		case button3:
 			set_PWM(329.63);
 			if(!A0 && !A1 && !A2){
-				PWM_off();
 				Note_state= silent;
 			}
 			else if( A0 && !A1 && !A2){
 				Note_state= button1;
-				PWM_on();
 			}
 			else if( !A0 && A1 && !A2){
 				Note_state= button2;
-				PWM_on();
 			}
 			else if( !A0 && !A1 && A2 ){
 				Note_state= button3;
-				PWM_on();
 			}
 			else if( (A0 + A1 + A2) > 1 ){
-				PWM_off();
 			        Note_state= silent;
 			}
 			break;
 		default:
-			Note _state= silent;
+			Note_state = silent;
 			break;
 	}
 }
@@ -156,15 +137,16 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0X00; PINA = 0XFF;
 	DDRB = 0X40; PORTB = 0x00;
-	
+
+	PWM_on();	
     /* Insert your solution below */
-	Note_state= init;
+	Note_state = init;
 	
-    while (1) {
+      while (1) {
 	A0 = ~PINA & 0x01;
 	A1 = (~PINA & 0x02) >> 1;
 	A2 = (~PINA & 0x04) >> 2;
 	Tick_Note();
-    }
+    } 
     return 1;
 }
