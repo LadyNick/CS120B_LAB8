df95ca5b93f16931e7e41c4dbb7aa8820cf627f2
LadyNick <nnikki1013@gmail.com>
2021-02-14 03:25:04 -0800

    added pwm functions

--- source/main.c
+++ source/main.c
@@ -12,6 +12,38 @@
 #include "simAVRHeader.h"
 #endif
 
+void set_PWM(double frequency){
+	static double current_frequency;
+
+	if(frequency != current_frequence){
+		if(!frequency) {TCCR3B &= 0X08;}
+		else { TCCR3B |= 0x03; }
+
+		if(frequency < 0.954) { OCR3A = 0XFFFF; }
+
+		else if(frequency > 31250) { OCR3A = 0X0000; }
+
+		else { OCR3A = (short) (8000000 / (128*frequency)) = 1; }
+
+		TCNT3 = 0;
+		current_frequency = frequency;
+
+	}
+}
+
+void PWM_on(){
+	TCCR3A = (1 << COM3A0);
+
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+
+	set_PWM(0);
+}
+
+void PWM_off(){
+	TCCR3A = 0X00;
+	TCCR3B = 0X00;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 
